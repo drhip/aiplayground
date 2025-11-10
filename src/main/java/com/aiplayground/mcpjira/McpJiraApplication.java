0bc9ce7c8e356ae242f366ddd00fdfc5f60df8a7
AI Teammate <firstaimate@gmail.com>
2025-11-10 22:23:47 +0200

    AIP-37: Set Up Project Directory Structure

---
+++ src/main/java/com/aiplayground/mcpjira/McpJiraApplication.java
@@ -0,0 +1,12 @@
+package com.aiplayground.mcpjira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpJiraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(McpJiraApplication.class, args);
+    }
+}
+
